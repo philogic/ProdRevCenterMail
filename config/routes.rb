fd063364377c120597a3a262945741ae69721ca1
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-01-03 11:49:40 +0100

    Add client to database seeds (and delete all previous records). Fix bug in routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :messages, except: %i[update show]
-  post 'authenticate', 'authentication#authenticate'
+  post 'authenticate', to: 'authentication#authenticate'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
+Message.delete_all
+Client.delete_all
+
 Message.create originator: 'James', recipient: 'Martin',
                content: 'My first message', status: 1
 Message.create originator: 'Marie', recipient: 'Patricia',
                content: 'Hello World', status: 3
+
+Client.create name: 'product-review-center', domain: 'www.prodrevcenter.com',
+              password: '5469secure'
